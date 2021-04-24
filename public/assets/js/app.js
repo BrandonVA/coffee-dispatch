2d4fb3e97914c0a3ca9b3cda34df0cb03e91487f
BrandonVA <codebva@gmail.com>
2021-04-24 15:44:09 -0700

    debugging and testing api/html routes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,14 @@
 const logoEl = document.getElementById("logo");
+const getDataBtn = document.getElementById("getData");
 
 logoEl.addEventListener("click", () => {
   alert("Hello World");
 });
+
+if (getDataBtn !== null) {
+  getDataBtn.addEventListener("click", () => {
+    fetch("/api/getData")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
+}
