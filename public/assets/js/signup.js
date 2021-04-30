b6c459f80c5d68417b2c193a7d2a39ef61804a7b
BrandonVA <codebva@gmail.com>
2021-04-29 22:05:59 -0700

    Added some password validation along with edited the tooltips

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -2,9 +2,25 @@ const signupBtn = document.getElementById("signupBtn");
 const signupForm = document.getElementById("signupForm");
 
 // signupBtn.addEventListener("click", () => {
-//   const data = signupForm;
-//   console.log();
+//   const data = validatePassword(signupForm.password.value);
+//   console.log(data);
 // });
+const validatePassword = (newPassword) => {
+  const passwordCriteria = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,}$/;
+  const test = passwordCriteria.test(newPassword);
+  console.log(test);
+  return test;
+};
+document
+  .getElementById("password")
+  .setAttribute(
+    "title",
+    "Password must be at least 8 characters with a number with an upper and lowercase letter"
+  );
+document
+  .getElementById("password")
+  .setAttribute("title", "Passwords must match");
+document.getElementById("email").setAttribute("title", "Must be a valid email");
 
 signupForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -17,19 +33,26 @@ signupForm.addEventListener("submit", function (event) {
     password: password.value.trim(),
   };
   console.log(newUser);
-  newUser.password !== confirmPassword.value
-    ? alert("passwords don't match")
-    : fetch("/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newUser),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          window.location.replace("/members");
+
+  if (!validatePassword(newUser.password)) {
+    alert(
+      "Password must be at least 8 characters with a number with an upper and lowercase letter"
+    );
+  } else {
+    newUser.password !== confirmPassword.value
+      ? alert("passwords don't match")
+      : fetch("/api/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newUser),
         })
-        .catch((err) => console.log(err));
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            window.location.replace("/members");
+          })
+          .catch((err) => console.log(err));
+  }
 });
