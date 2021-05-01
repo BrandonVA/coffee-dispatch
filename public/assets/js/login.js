4c4b6b476b6bee39f08c64a19c9ff4704d189ff2
BrandonVA <codebva@gmail.com>
2021-05-01 16:57:44 -0700

    Added click event to handle changing input type of the password field

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,17 +1,33 @@
 const loginBtn = document.getElementById("loginBtn");
 const loginForm = document.getElementById("loginForm");
+const passwordBtn = document.querySelectorAll(".btn-show-pass");
 
 // loginBtn.addEventListener("click", () => {
 //   const data = loginForm;
 //   console.log();
 // });
+const showPassword = (e) => {
+  e.preventDefault();
+  const icon = e.target;
+  if (icon.classList.contains("fa-eye-slash") === true) {
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+    e.target.parentElement.previousElementSibling.previousElementSibling.type =
+      "text";
+  } else {
+    icon.classList.remove("fa-eye");
+    e.target.parentElement.previousElementSibling.previousElementSibling.type =
+      "password";
+    icon.classList.add("fa-eye-slash");
+  }
+};
+passwordBtn.forEach((el) => {
+  el.addEventListener("click", showPassword);
+});
 
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const { email, password } = this.elements;
-  //   console.log(this.elements);
-  //   console.log(email, password, confirmPassword);
-  //   console.log(email.value, password.value, confirmPassword.value);
   const user = {
     email: email.value,
     password: password.value,
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,33 +1,42 @@
 const signupBtn = document.getElementById("signupBtn");
 const signupForm = document.getElementById("signupForm");
+const passwordBtn = document.querySelectorAll(".btn-show-pass");
 
 // signupBtn.addEventListener("click", () => {
 //   const data = validatePassword(signupForm.password.value);
 //   console.log(data);
 // });
+
+const showPassword = (e) => {
+  e.preventDefault();
+  const icon = e.target;
+  if (icon.classList.contains("fa-eye-slash") === true) {
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+    e.target.parentElement.previousElementSibling.previousElementSibling.type =
+      "text";
+  } else {
+    icon.classList.remove("fa-eye");
+    e.target.parentElement.previousElementSibling.previousElementSibling.type =
+      "password";
+    icon.classList.add("fa-eye-slash");
+  }
+};
+
+passwordBtn.forEach((el) => {
+  el.addEventListener("click", showPassword);
+});
+
 const validatePassword = (newPassword) => {
   const passwordCriteria = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,}$/;
   const test = passwordCriteria.test(newPassword);
   console.log(test);
   return test;
 };
-document
-  .getElementById("password")
-  .setAttribute(
-    "title",
-    "Password must be at least 8 characters with a number with an upper and lowercase letter"
-  );
-document
-  .getElementById("password")
-  .setAttribute("title", "Passwords must match");
-document.getElementById("email").setAttribute("title", "Must be a valid email");
 
 signupForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const { email, password, confirmPassword } = this.elements;
-  //   console.log(this.elements);
-  //   console.log(email, password, confirmPassword);
-  //   console.log(email.value, password.value, confirmPassword.value);
   const newUser = {
     email: email.value.trim(),
     password: password.value.trim(),
@@ -56,3 +65,15 @@ signupForm.addEventListener("submit", function (event) {
           .catch((err) => console.log(err));
   }
 });
+
+// Adding new tooltip msg
+document
+  .getElementById("password")
+  .setAttribute(
+    "title",
+    "Password must be at least 8 characters with a number with an upper and lowercase letter"
+  );
+document
+  .getElementById("confirmPassword")
+  .setAttribute("title", "Passwords must match");
+document.getElementById("email").setAttribute("title", "Must be a valid email");
